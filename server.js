f77412a8ae94fff35e62e16f135501ca950ffe4f
EcjTn <ecjtaneo@gmail.com>
2025-05-29 14:09:50 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express()
+
+app.get("/", (req, res) => {
+    res.json({"message": "Hello Main"});
+})
+
+
+
+const userRoute = require("./user/users")
+
+
+
+app.use("/users", userRoute)
+
+
+
+
+
+
+
+app.listen(8000)
