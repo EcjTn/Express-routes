84d7eb57d697d42ecc5a5836cef9c9fc3cf084fe
EcjTn <ecjtaneo@gmail.com>
2025-06-02 12:47:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ app.get("/", (req, res) => {
     res.json({"message": "Hello Main"});
 })
 
-app.get("/main", (req, res) => {
-    res.send("this is main");
-})
-
 const userRoute = require("./user/users")
 
 app.use("/users", userRoute)
