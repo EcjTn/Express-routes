ecad1c8f50cb6d002a69b79b2e98cb21993395ec
EcjTn <ecjtaneo@gmail.com>
2025-06-13 20:22:28 +0800

    Add files via upload

---
+++ database/table.js
@@ -0,0 +1,10 @@
+const users = [
+  { username: "john_doe", password: "password123" },
+  { username: "alice_smith", password: "qwerty456" },
+  { username: "charlie99", password: "abc123" },
+  { username: "admin", password: "admin" }, // admin account
+  { username: "emma_21", password: "password21" }
+];
+
+
+export { users };
---
+++ main.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import { users } from './database/table.js'
+const app = express()
+
+app.use(express.json())
+
+
+
+app.get('/', (req,res) => {
+    res.json("Welcome")
+})
+
+app.get('/database', (req,res) => {
+    res.send(users)
+})
+
+
+
+
+
+app.post('/admin', (req,res) => {
+    let checkAdmin = req.body
+    const userData = users.find(data => data.username === checkAdmin.username && data.password === checkAdmin.password)
+    console.log(userData)
+
+    if(!userData){return res.send("no found")}
+
+    if(userData){
+        console.log("[  *  ] Admin logged in")
+    }
+
+
+})
+
+
+
+
+app.listen(8000, () => console.log("[*] Streaming on port 8000"))
